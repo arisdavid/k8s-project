00ec80a8204c81663d3e16221e68708e99db42c7
arisdavid3 <aris.david@gmail.com>
2020-03-28 15:21:56 +0000

    Added pre-commit hook and config

--- consumer/consumer_app.py
+++ consumer/consumer_app.py
@@ -1,12 +1,12 @@
 import argparse
+import logging
 import os
 
 from pykafka import KafkaClient
-import logging
 
 logging.basicConfig(level=logging.INFO)
 
-client = KafkaClient(hosts=os.getenv('KAFKA_HOST'))
+client = KafkaClient(hosts=os.getenv("KAFKA_HOST"))
 
 
 def get_message(topic):
@@ -20,12 +20,12 @@ def get_message(topic):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser("Get messages from a kafka topic")
-    parser.add_argument('topic', help='kafka topic')
+    parser.add_argument("topic", help="kafka topic")
     args = parser.parse_args()
 
     _topic = str(args.topic)
 
     if _topic is None:
-        raise Exception('Please make sure to provide topic')
+        raise Exception("Please make sure to provide topic")
 
     get_message(_topic)
